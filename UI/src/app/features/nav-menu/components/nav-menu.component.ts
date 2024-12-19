a41492c18c387b14facbe9d76f26854cad10e37b
Kristina-Silvanovych <64775345+Kristina-Silvanovych@users.noreply.github.com>
2024-12-19 14:22:33 +0200

    nav-menu

---
+++ UI/src/app/features/nav-menu/components/nav-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component, Inject } from '@angular/core';
+
+@Component({
+    selector: 'nav-menu',    
+    templateUrl: './nav-menu.component.html',
+    styleUrls: ['./nav-menu.component.css']
+})
+export class NavMenuComponent {
+    
+}
